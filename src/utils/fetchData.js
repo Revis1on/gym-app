59c95b25322613258c298f31ff89a1e525a01b9b
Filip Risteski <38330826+Revis1on@users.noreply.github.com>
2022-06-13 17:47:11 +0200

    ver 1.0

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": "6e12838775msh91f9fbfb9dd9c28p16d601jsnbfa3453a22fa",
+  },
+};
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": "6e12838775msh91f9fbfb9dd9c28p16d601jsnbfa3453a22fa",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
